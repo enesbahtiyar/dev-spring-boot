f1df6117923a5251703ecabd721bae819eda8067
Enes Bahtiyar <97892255+enesbahtiyar@users.noreply.github.com>
2023-07-05 12:57:52 +0300

    Initial commit

---
+++ app/src/main/java/eb/springboot/demo/app/AppApplication.java
@@ -0,0 +1,15 @@
+package eb.springboot.demo.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppApplication
+{
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(AppApplication.class, args);
+	}
+
+}
